6a93ac16ae48434b8be3143a37d843ddeed10583
Erik <erik.ra@gmail.com>
2017-04-11 23:45:56 +0200

    Add project files.

---
+++ Trik/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trik
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool leftCup = true;
+            bool middleCup = false;
+            bool rightCup = false;
+            bool temp = false;
+
+            foreach (var c in Console.ReadLine())
+            {
+                switch (c)
+                {
+                    case 'A': temp = middleCup; middleCup = leftCup; leftCup = temp; break;
+                    case 'B': temp = middleCup; middleCup = rightCup; rightCup = temp; break;
+                    case 'C': temp = rightCup; rightCup = leftCup; leftCup = temp; break;
+                }
+            }
+            if (leftCup)
+            {
+                Console.WriteLine(1);
+            }
+            else if (middleCup)
+            {
+                Console.WriteLine(2);
+            }
+            else if (rightCup)
+            {
+                Console.WriteLine(3);
+            }
+        }
+    }
+}
